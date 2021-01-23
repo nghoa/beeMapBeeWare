42d3a05b90116a0609c78b6b76af2b505137dff6
nghoa <nghoa@hotmail.de>
2021-01-23 11:07:20 +0200

    starting restructuring & refactoring

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile('dev.cfg')
+    register_blueprints(app)
+
+    return app
+
+def register_blueprints(app):
+    # Since the application instance is now created, register each Blueprint
+    # with the Flask application instance (app)
+    from app.map import map_blueprint
+ 
+    app.register_blueprint(map_blueprint)
---
+++ app/map.py
@@ -0,0 +1,67 @@
+import os
+
+from flask import Flask, session, redirect, url_for, escape, request, Response, render_template, make_response, jsonify
+
+from src.bee_service import get_fields, getLocations, save_suggestion, validateForm
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('map.html')
+
+
+def parseSuggestion():
+    """
+    Parses json into fields
+    """
+    fields = request.get_json()
+    #app.logger.debug(fields)
+
+    return fields
+
+
+@app.route("/save", methods=["POST"])
+def save():
+    """
+    Save received bee-village suggestion
+    Response:
+        message telling if the suggestion was valid
+    """
+    fields = parseSuggestion()
+
+    errors = validateForm(fields)
+    if len(errors) == 0:
+        save_suggestion(fields)
+        status_code = 200
+    else:
+        status_code = 400 #bad request
+    
+    return jsonify(errors), status_code 
+    
+
+@app.route("/locations", methods=["GET", "POST"])
+def locations():
+    locations = getLocations()
+
+    json_data = jsonify(locations)
+
+    # make the response from json data requiderd by Flask. As HTTP code is used 200, OK
+    resp = make_response(json_data, 200)
+    resp.charset = "UTF-8"              # the charset of the response
+    resp.mimetype = "application/json"  # the media type of the response
+
+    return resp
+
+
+def start_app():
+    host = os.getenv("HOST", "127.0.0.1")
+    port = os.getenv("PORT", "5000")
+
+    app.run(host=host, port=port)
+
+
+if __name__ == "__main__":
+    start_app()
+
