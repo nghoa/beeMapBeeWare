b59b58fa0d1ce88c8976667610efd11287df2ba1
nghoa <nghoa@hotmail.de>
2021-01-24 15:36:20 +0200

    starting script rewritten

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import os
 from app import create_app
 
 # Call the Application Factory function to construct a Flask application instance
 # using the standard configuration defined in /instance/dev.cfg
-app = create_app()
+
+def start_app():
+    app = create_app()
+    host = os.getenv('HOST', '127.0.0.1')
+    port = os.getenv('PORT', '5000')
+
+    app.run(host=host, port=port)
 
 if __name__ == '__main__':
-    app.run()
+    start_app()
+
