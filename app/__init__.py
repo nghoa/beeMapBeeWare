741b783792019c88268864e1f2b9808b51204e54
nghoa <nghoa@hotmail.de>
2021-01-24 18:36:10 +0200

    add init in tests

--- app/__init__.py
+++ app/__init__.py
@@ -9,14 +9,12 @@ def create_app():
 def register_blueprints(app):
     # Since the application instance is now created, register each Blueprint
     # with the Flask application instance (app)
-    from app.home import home_blueprint
     from app.map import map_blueprint
     from app.admin import admin_blueprint
 
     from app.services.auth import auth_blueprint
  
-    app.register_blueprint(home_blueprint)
     app.register_blueprint(auth_blueprint)
+    app.register_blueprint(map_blueprint)
 
-    app.register_blueprint(map_blueprint, url_prefix='/map')
     app.register_blueprint(admin_blueprint, url_prefix='/admin')    
--- app/home/views.py
+++
@@ -1,5 +0,0 @@
-from . import home_blueprint
-
-@home_blueprint.route('/')
-def hello():
-    return 'Home page'
