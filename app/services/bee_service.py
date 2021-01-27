48b3ffaa8efcf515dcb770bf7b23f9bfeb7b3cf1
Aleksi Tarvainen <a.aleksi.tarvainen@gmail.com>
2021-01-27 19:05:43 +0200

    Apply some polish to validation and add docstrings

--- app/services/bee_service.py
+++ app/services/bee_service.py
@@ -1,6 +1,46 @@
-class ErrorMessages:
-    MISSING = "Missing value"
-    NOT_REAL = "Must be real number"
+from enum import Enum
+from typing import Optional, Dict
+
+ValidationError = str  # use strings to represent validation errors at least for now
+
+
+class FieldType(Enum):
+    """
+    Form field types that are used for validating fields' values.
+    Their values (FieldType.*.value) should be understandable.
+    """
+    FLOAT = "float"
+    NONEMPTY_STRING = "nonempty string"
+    EMAIL = "email"
+
+
+class ErrorMessage:
+    """
+    Contains methods for creating error messages.
+    """
+    @staticmethod
+    def missing_value() -> ValidationError:
+        """
+        Return this if field's value is missing (None).
+
+        Returns:
+            ValidationError: message describing the validation error
+        """
+        return "Missing value"
+
+    @staticmethod
+    def type_mismatch(field_type: FieldType, value) -> ValidationError:
+        """
+        Return this if the field's actual type doesn't match the intended.
+
+        Args:
+            field_type (FieldType): that was expected
+            value ([type]): which was isn't of the expected type
+
+        Returns:
+            ValidationError: message describing the validation error
+        """
+        return f"Expected a value of type '{field_type.value}', was {value}"
 
 
 def get_fields(form, fields):
@@ -12,35 +52,58 @@ def get_fields(form, fields):
     Returns:
         dict[str, str] dictionary of field names and values 
     """
-    #app.logger.debug(form.get("name"))
-    return {name: form.get("name", default) for (name,default) in fields}
+    # app.logger.debug(form.get("name"))
+    return {name: form.get("name", default) for (name, default) in fields}
 
-def validateValue(fields, field, isFloat = False):
-    if not field in fields:
-        return ErrorMessages.MISSING
-    if not fields[field]:
-        return ErrorMessages.MISSING
-    if isFloat:
+
+def validateValue(fields, field: str, field_type: FieldType) -> Optional[ValidationError]:
+    """
+    Validate a value in fields with validation rules for given field type.
+
+    Args:
+        fields: fields from form
+        field (str): field to be validated
+        field_type (FieldType): type of field
+
+    Returns:
+        Optional[ValidationError]: None if no error. Validation error if value is 
+        invalid.
+    """
+    value = fields.get(field, None)
+    if not value:
+        return ErrorMessage.missing_value()
+    if field_type == FieldType.FLOAT:
         try:
-            float(fields[field])
+            float(value)
         except:
-            return ErrorMessages.NOT_REAL
+            return ErrorMessage.type_mismatch(FieldType.FLOAT, value)
+    elif field_type == FieldType.NONEMPTY_STRING:
+        if value != "":
+            return None
+        else:
+            return ErrorMessage.type_mismatch(FieldType.NONEMPTY_STRING, value)
     return None
 
-def validateForm(fields):
+
+def validateForm(fields) -> Dict[str, ValidationError]:
     """
-    Checks that fields are valid
-    TODO: write validations for form fields
+    Validate form fields' contents. Each field gets their own validation error message
+    if any.
     Params:
         fields: {"field name", "field value"}
     Returns:
         {"field name": "error message"}
     """
     errors = {}
-    requiredFields = [("name", False), ("latitude", True), ("longitude", True)]
+    requiredFields = [
+        ("name", FieldType.NONEMPTY_STRING),
+        ("latitude", FieldType.FLOAT),
+        ("longitude", FieldType.FLOAT),
+    ]
+
     for field, isFloat in requiredFields:
         error = validateValue(fields, field, isFloat)
         if error is not None:
             errors[field] = error
-    
+
     return errors
