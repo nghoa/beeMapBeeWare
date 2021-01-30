bae0eb8472ecfbd861ae7631163442379c830985
Juha Reinikainen <juha.reinikainen1@gmail.com>
2021-01-30 19:27:19 +0200

    moved config reading to top level

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from app import create_app
 # using the standard configuration defined in /instance/dev.cfg
 
 app = create_app()
+app.config.from_pyfile("instance_config.py")
 
 def start_app():
     host = os.getenv('HOST', '127.0.0.1')
@@ -12,7 +13,6 @@ def start_app():
     
     #load secret key from file, create this file yourself
     #https://gitlab.jyu.fi/startuplab/courses/tjts5901-continuous-software-engineering/beemaptemplate/-/blob/master/docs/adding-flask-config-file.md
-    app.config.from_pyfile("instance_config.py")
     app.run(host=host, port=port)
     
 if __name__ == '__main__':
