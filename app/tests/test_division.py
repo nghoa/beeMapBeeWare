aa5dd025e1a7ea25835086e271d22a1d5b178126
Markus Koskela <majuketo@gstudent.jyu.fi>
2021-02-03 20:24:48 +0200

    Logging optimization to test, too

--- app/tests/test_division.py
+++ app/tests/test_division.py
@@ -7,19 +7,16 @@ from flask import Flask
 app = create_app()
 app.config["TESTING"] = True
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+import logging
 
-# Azure loghandler.
-connection_string = "InstrumentationKey=bfe6d9a0-78dc-40fb-a307-b0d8c97bc266;IngestionEndpoint=https://northeurope-0.in.applicationinsights.azure.com/"
-logger.addHandler(AzureLogHandler(connection_string=connection_string))
+logger = logging.getLogger("main_logger")
 
 def test_division_by_zero():
 
     """
         Test for division by zero
     """
-    number = 3
+    number = 2
 
     result = -1
     try:
