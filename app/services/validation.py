e4c18fc02281fe00297b89095057c3ab0456bd8c
Juha Reinikainen <juha.reinikainen1@gmail.com>
2021-02-05 23:34:05 +0200

    Changed from dicts to class Now now float parsable strings in lat,lng in form don't throw errors in javascript

--- app/services/validation.py
+++ app/services/validation.py
@@ -1,21 +1,21 @@
-from flask_wtf import FlaskForm
 from wtforms import Form, StringField, FloatField
 from wtforms.validators import InputRequired, NumberRange
-from flask import request
-from enum import Enum
-from typing import Optional, Dict
 
 
 class ErrorMessage:
     REQUIRED = "This field is required"
-    LATITUDE = "Latitude has to between -90 and 90"
+    LATITUDE = "Latitude has to be between -90 and 90"
     LONGITUDE = "Longitude has to be between -180 and 180"
 
 
 class SuggestionForm(Form):
-    name = StringField('name', validators=[
-                       InputRequired(message=ErrorMessage.REQUIRED)])
+    firstname = StringField('firstname', validators=[
+                        InputRequired(message=ErrorMessage.REQUIRED)])
+    lastname = StringField("lastname", validators=[
+                        InputRequired(message=ErrorMessage.REQUIRED)])
     latitude = FloatField("latitude", validators=[
-                          InputRequired(message=ErrorMessage.REQUIRED), NumberRange(-90, 90, message=ErrorMessage.LATITUDE)])
+                        InputRequired(message=ErrorMessage.REQUIRED), NumberRange(-90, 90, message=ErrorMessage.LATITUDE)])
     longitude = FloatField("longitude", validators=[
-                           InputRequired(message=ErrorMessage.REQUIRED), NumberRange(-180, 180, message=ErrorMessage.LONGITUDE)])
+                        InputRequired(message=ErrorMessage.REQUIRED), NumberRange(-180, 180, message=ErrorMessage.LONGITUDE)])
+    email = StringField("email", validators=[
+                        InputRequired(message=ErrorMessage.REQUIRED)])
